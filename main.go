3e3e469aaab204d765c2ee9e877392a25f020ecc
Davis Davalos-DeLosh <davis.davalos.delosh@gmail.com>
2017-07-07 22:08:30 -0600

    first commit

---
+++ main.go
@@ -0,0 +1,86 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"os/exec"
+
+	"strings"
+
+	"github.com/rakyll/launchpad"
+)
+
+var hitX int
+var hitY int
+var bindings map[string]string
+
+func main() {
+	learn()
+	pad, err := launchpad.Open()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer pad.Close()
+
+	pad.Clear()
+
+	ch := pad.Listen()
+	for {
+		select {
+		case hit := <-ch:
+			fmt.Printf("%v, %v\n", hit.X, hit.Y)
+			if _, ok := bindings[string(hit.X)+string(hit.Y)]; ok {
+				output, err := exec.Command("bash", "-c", bindings[string(hit.X)+string(hit.Y)]).CombinedOutput()
+				if err != nil {
+					os.Stderr.WriteString(err.Error())
+				}
+				fmt.Println(string(output))
+			}
+			pad.Clear()
+			pad.Light(hit.X, hit.Y, 3, 3)
+		}
+	}
+}
+func learn() {
+	keypressed := false
+	fmt.Println("Learning mode!")
+	fmt.Println("Please press the button that you would like to bind")
+	pad, err := launchpad.Open()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer pad.Close()
+
+	pad.Clear()
+
+	ch := pad.Listen()
+	for !keypressed {
+		select {
+		case hit := <-ch:
+			fmt.Printf("%v, %v\n", hit.X, hit.Y)
+			hitX = hit.X
+			hitY = hit.Y
+			pad.Clear()
+			pad.Light(hit.X, hit.Y, 3, 3)
+			keypressed = true
+		}
+	}
+	fmt.Println("What command in your default shell would you like to bind this to?")
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Print("Enter text: ")
+	text, _ := reader.ReadString('\n')
+	fmt.Printf("You want to bind %s to %v, %v?\n", text, hitX, hitY)
+	yn, _ := reader.ReadString('\n')
+	yn = strings.ToUpper(yn)
+	if yn == "N" {
+		learn()
+	}
+	bindings[string(hitX)+string(hitY)] = text
+	fmt.Println("Complete!")
+}
+
+func init() {
+	bindings = make(map[string]string)
+}
